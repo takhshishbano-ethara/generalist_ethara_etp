70d7fadba1d813cbbd9e5492ff0e26118342e297
piyush <piyush.agrawal@kuberha.com>
2026-03-17 15:36:17 +0530

    feat: add valor test cases

---
+++ custom_addons/valor/tests/__init__.py
@@ -0,0 +1,6 @@
+from . import test_valor_turn_history
+from . import test_valor_turn_submit
+from . import test_valor_turn_evaluate
+from . import test_valor_model
+from . import test_valor_media_and_qc
+
---
+++ custom_addons/valor/tests/test_valor_turn.py
@@ -0,0 +1,8 @@
+"""Legacy test module kept empty after split.
+
+Tests have been moved into:
+- test_valor_turn_history.py
+- test_valor_turn_submit.py
+- test_valor_turn_evaluate.py
+- test_valor_model.py
+"""
