514bf0d252fc98ebb854e5dc53dd55c0f3808ea0
sarbesh <sarbeshtiwari39@gmail.com>
2026-06-08 18:54:38 +0530

    feat:added  kensei extension

---
+++ custom_addons/kensei_extension/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Kensei Extension",
+    "version": "19.0.1.0.0",
+    "category": "Tools",
+    "summary": "Role-scoped analytics dashboard REST API for Kensei2 tasks.",
+    "author": "Ethara",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "web",
+        "kensei2",
+        "api_auth_gateway",
+        "task_forge_bridge",
+        "project_extension",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
