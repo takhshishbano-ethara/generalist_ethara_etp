3541e3103ae8e80e52bc6b704164bab421cbcf65
takhshishbano-ethara <takhshish.bano@ethara.ai>
2026-06-11 18:31:28 +0530

    batch

--- custom_addons/crowley_extension/controllers/__init__.py
+++ custom_addons/crowley_extension/controllers/__init__.py
@@ -1,5 +1,6 @@
 from . import analytics_dashboard
 from . import task_view_dashboard
+from . import batch_dashboard
 from . import dashboard_overview
 from . import reference_data
 from . import budget
